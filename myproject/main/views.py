ff3de4eebf43e1773ef65615a5ae3568e5060fe4
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2021-07-24 16:45:22 +0900

    complete CRUD

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -24,3 +24,21 @@ def create(request):
     new_blog.pub_date = timezone.now()
     new_blog.save()
     return redirect('detail', new_blog.id)
+
+def edit(request, id):
+    edit_blog = Blog.objects.get(id=id)
+    return render(request, 'edit.html', {'blog': edit_blog})
+
+def update(request, id):
+    update_blog = Blog.objects.get(id=id)
+    update_blog.title = request.POST['title']
+    update_blog.writer = request.POST['writer']
+    update_blog.body = request.POST['body']
+    update_blog.pub_date = timezone.now()
+    update_blog.save()
+    return redirect('detail', update_blog.id)
+
+def delete(request, id):
+    delete_blog = Blog.objects.get(id=id)
+    delete_blog.delete()
+    return redirect('home')
